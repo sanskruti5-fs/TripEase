3d13a0ce0c9b2f63d6b34f42db10377c1a6cebc8
sanskruti5-fs <sanskruti.work7@gmail.com>
2026-04-17 05:02:16 +0530

    Delete backend directory

--- backend/debug_curl.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-try {
-    const url = "https://loremflickr.com/800/600/Udaipur,Pyaaz,Kachori";
-    const dest = "C:\\TripEase\\frontend\\public\\images\\udaipur\\test.jpg";
-    execSync(`curl.exe -L -k -s -v -o "${dest}" "${url}"`, { stdio: 'inherit' });
-    console.log("Success");
-} catch (err) {
-    console.error("Error:", err);
-}
